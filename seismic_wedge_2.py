b4040058c5a1544d14d3c225b1080de3ad26f3eb
Murray Hoggett <murrayhoggett@gmail.com>
2018-03-22 10:01:09 +0000

    Before rerunning resolution graphs

--- seismic_wedge_2.py
+++ seismic_wedge_2.py
@@ -16,8 +16,8 @@ import bruges
 from skimage.util import random_noise as random_noise
 
 ## Choose the dimensions of the model, the angle of the wedge, and the vels.
-length, depth = 10000, 450
-theta = 1.75
+length, depth = 5000, 450 # length based on average sill diam of 10000m, so 5000m radius.
+theta = 2.9 # based on arctan(250/5000)
 dt = 0.001
 rocks = np.array([[1750, 2100],  # Vp, rho
                   [6000, 2900],
@@ -85,8 +85,8 @@ def make_wedge(width, depth, l1t, theta, dt, noise=False, noise_var=0.1):
         time_rc_model = np.divide(np.add(time_rc_model, time_rc_noise),2)
     return depth_rc_model, earth, imp, time_vec, time_rc_model
     
-depth_rc_model, earth, imp, time_vec, time_rc_model = make_wedge(width=length,depth=depth,l1t=depth/4,theta=theta, dt=dt, noise=False, noise_var=0.1)
-depth_rc_model_n, earth, imp, time_vec, time_rc_model_n = make_wedge(width=length,depth=depth,l1t=depth/4,theta=theta, dt=dt, noise=True, noise_var=0.05)
+depth_rc_model, earth, imp, time_vec, time_rc_model = make_wedge(width=length,depth=depth,l1t=depth/4,theta=theta, dt=dt, noise=False, noise_var=0.01)
+depth_rc_model_n, earth, imp, time_vec, time_rc_model_n = make_wedge(width=length,depth=depth,l1t=depth/4,theta=theta, dt=dt, noise=True, noise_var=0.01)
 
 
 # Plot depth rc
@@ -126,13 +126,15 @@ w = bruges.filters.ricker(duration=0.100, dt=dt, f=freq)
 # Plots the ricker wavelet 
 plt.plot(w)
 plt.title('Ricker wavelet, peak frequency=%iHz' %freq)
+plt.grid()
 plt.show()
 
+
+##### Plot the clean wedge model ######
 # Convolve time reflection coefficient matrix with the ricker wavelet to make synthetic
 synth = np.apply_along_axis(lambda t: np.convolve(t, w, mode='same'),
                             axis=0,
-                            arr=time_rc_model_n)
-
+                            arr=time_rc_model)
 # Plot synthetic seismic
 plt.imshow(synth, cmap="seismic", aspect='auto', extent=(0, length, np.max(time_vec), 0))
 plt.title('Synthetic seismic wedge model for a sill')
@@ -164,7 +166,7 @@ plt.grid()
 plt.ylim(0,np.nanmax(apparent2))
 plt.xlim(0,length)
 plt.legend()
-#plt.savefig('ch2_synthetic_amplitude_curve2.jpg', dpi=300)
+#plt.savefig('ch2_synthetic_amplitude_curve_clean.jpg', dpi=300)
 plt.show()
 
 # Find the locations of the maximum amplitudes for the top surface
@@ -181,10 +183,9 @@ plt.colorbar().set_ticks([])
 plt.plot(apparent_t*dt, 'g-', label='Apparent amplitude') # Note *dt. 
 plt.plot(actual_t*dt, 'y-', label='Actual amplitude') # Note *dt.
 plt.xlabel('Distance (m)')
-#plt.ylabel('Two way travel time (s)')
 plt.ylabel('Two way time (s)')
 plt.legend()
-#plt.savefig('ch2_synthetic_wedge_seismic2.jpg', dpi=300)
+#plt.savefig('ch2_synthetic_wedge_seismic_clean.jpg', dpi=300)
 plt.show()
 
 # Four axes, returned as a 2-d array
@@ -198,10 +199,10 @@ plt.ylabel('Depth (m)')
 axarr[0, 1].set_title(r'$\rho$')
 m3 = axarr[1, 0].imshow(imp,  cmap='magma', aspect='auto')
 f.colorbar(m3, ax=axarr[1, 0]).ax.set_ylabel(r'$ai \/\/ (Pa. s/m^3)$')
-axarr[1, 0].set_title('Acoustic imp.')
+axarr[1, 0].set_title('Acoustic impedance')
 m4 = axarr[1, 1].imshow(depth_rc_model, cmap='Greys', aspect='auto')
 f.colorbar(m4, ax=axarr[1, 1]).ax.set_ylabel(r'$RC$')
-axarr[1, 1].set_title('Reflection coeff.')
+axarr[1, 1].set_title('Reflection coefficient')
 # Set common axes labels 
 f.text(0.5, 0.04, 'Distance (m)', ha='center')
 f.text(0.04, 0.5, 'Depth (m)', va='center', rotation='vertical')
@@ -210,5 +211,64 @@ plt.setp([a.get_xticklabels() for a in axarr[0, :]], visible=False)
 plt.setp([a.get_yticklabels() for a in axarr[:, 1]], visible=False)
 fig = plt.gcf()
 fig.set_size_inches(10, 7)
-#plt.savefig('ch2_synthetic_wedge_setup2.jpg', dpi=300)
+#plt.savefig('ch2_synthetic_wedge_setup_clean.jpg', dpi=300)
+plt.show()
+
+##### Plot the noisy wedge model ######
+# Convolve time reflection coefficient matrix with the ricker wavelet to make synthetic
+synth_noisy = np.apply_along_axis(lambda t: np.convolve(t, w, mode='same'),
+                            axis=0,
+                            arr=time_rc_model_n)
+# Plot synthetic seismic
+plt.imshow(synth_noisy, cmap="seismic", aspect='auto', extent=(0, length, np.max(time_vec), 0))
+plt.title('Synthetic seismic wedge model for a sill with noise')
+plt.xlabel('Distance (m)')
+plt.ylabel('Two way travel time (s)')
+plt.colorbar().set_ticks([])
+plt.show()
+
+# We can exploit the fact that the reflections have the largest and smallest
+# values at the top and bottom of the wedge, to find the amplitudes of the reflections
+# at the peak. 
+# Find the max of the columns (corresponding to the reflection at the top of the sill)
+top_sill = int((((depth/4) / rocks[0,0] ) * 2) / dt)
+apparent = np.amax(synth_noisy[top_sill-10:top_sill+10,:], axis=0)
+actual = synth_noisy[top_sill, :]
+background = np.average(np.absolute(synth_noisy[0:9,:]), axis=0)
+import pandas as pd
+apparent2 = pd.rolling_mean(apparent, 50)
+actual2 = pd.rolling_mean(actual, 50)
+background2 = pd.rolling_mean(background, 50)
+
+plt.figure(figsize=(10,7))
+plt.plot(apparent2, 'g-', label='Apparent amplitude')
+plt.plot(actual2, 'y-', label='Actual amplitude')
+plt.plot(background2, 'b-', label='Background average amplitude')
+plt.ylabel('Amplitude')
+plt.xlabel('Distance (m)')
+plt.grid()
+plt.ylim(0,np.nanmax(apparent2))
+plt.xlim(0,length)
+plt.legend()
+#plt.savefig('ch2_synthetic_amplitude_curve_noisy.jpg', dpi=300)
+plt.show()
+
+# Find the locations of the maximum amplitudes for the top surface
+apparent_t = np.argmax(synth_noisy[top_sill-10:top_sill+10,:], axis=0) 
+apparent_t += (top_sill-10)
+apparent_t = apparent_t.astype(float)
+apparent_t[apparent_t==0]=np.nan
+actual_t = np.repeat(top_sill, length)
+
+# Plot seismic 
+plt.figure(figsize=(10,7))
+plt.imshow(synth_noisy, aspect='auto', cmap='seismic', extent=(0, length, np.max(time_vec), 0))
+plt.colorbar().set_ticks([])
+# Plot location of peak amplitude
+plt.plot(apparent_t*dt, 'g-', label='Apparent amplitude') # Note *dt. 
+plt.plot(actual_t*dt, 'y-', label='Actual amplitude') # Note *dt.
+plt.xlabel('Distance (m)')
+plt.ylabel('Two way time (s)')
+plt.legend()
+#plt.savefig('ch2_synthetic_wedge_seismic_noisy.jpg', dpi=300)
 plt.show()
